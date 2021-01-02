a24f251e5c5d757b31184640a71887dbde2613c3
Vladislav Ivanov <ivanovlad2010@gmail.com>
2021-01-02 14:33:55 +0300

    mikro orm set up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world');
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+
+    const posts = await orm.em.find(Post, {});
+    console.log(posts);
+};
+
+main().catch((err) => {
+    console.error(err);
+});
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,17 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import path from 'path';
+
+const microConfig = {
+    migrations: {
+        path: path.join(__dirname, './migrations'),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    entities: [Post],
+    dbName: 'unreddit',
+    debug: !__prod__,
+    type: 'postgresql',
+} as Parameters<typeof MikroORM.init>[0];
+
+export default microConfig;
